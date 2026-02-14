ac253956b3f716a425fb848356f9ac0e69a85033
letuandung01012008qn-spec <letuandung01012008qn@gmail.com>
2026-02-14 23:31:12 +0700

    Add files via upload

---
+++ petals.js
@@ -0,0 +1,21 @@
+﻿function createPetal(){
+
+const petal=document.createElement("div");
+
+petal.innerHTML="✨";
+petal.classList.add("petal");
+
+petal.style.left=Math.random()*100+"vw";
+petal.style.animationDuration=(5+Math.random()*5)+"s";
+
+document.body.appendChild(petal);
+
+setTimeout(()=>{
+
+petal.remove();
+
+},10000);
+
+}
+
+setInterval(createPetal,400);
